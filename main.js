57d22d5a7740ed295fd0fdc3c3a86a3a436d67e9
Dino Tudor <dinotudor@gmail.com>
2019-06-25 13:25:59 +0200

    onMousedown event

--- main.js
+++ main.js
@@ -3,15 +3,23 @@ $(document).ready(function() {
   paper.install(window);
   paper.setup(document.getElementById('mainCanvas'));
 
-  var c;
-  for(var x=25; x<400; x+=50) {
-    for(var y=25; y<400; y+=50) {
-      c = Shape.Circle(x, y, 10);
-      c.fillColor = 'green';
-    }
-  }
+  var tool = new Tool();
 
-  console.log(c);
+
+
+  tool.onMouseDown = function(event) {
+    var c = Shape.Circle(event.point.x, event.point.y, 20);
+    c.fillColor = 'green';
+  };
+
   paper.view.draw();
 
 });
+
+// LOOP TO DRAW CIRCLES EVENLY SPACED
+
+/* var c;
+for(var x=25; x<400; x+=50) {
+  for(var y=25; y<400; y+=50) {
+    c = Shape.Circle(x, y, 10);
+ */
